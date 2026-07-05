ee08360661c7553ca7ec19941733784aad8a460c
NedaFarrokhzad <66213038+NedaFarrokhzad@users.noreply.github.com>
2026-07-05 23:57:24 +0200

    Add files via upload

---
+++ run_keyword_analysis.py
@@ -0,0 +1,7 @@
+from src.keyword_analysis import run_analysis
+from src.keyword_visualization import run_visualization
+
+
+if __name__ == "__main__":
+    run_analysis()
+    run_visualization()
---
+++ run_sentiment_analysis.py
@@ -0,0 +1,7 @@
+from src.sentiment_analysis import run_analysis
+from src.sentiment_visualization import run_visualization
+
+
+if __name__ == "__main__":
+    run_analysis()
+    run_visualization()
