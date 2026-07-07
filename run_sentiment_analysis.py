0d611d1327716d3ef1976f923558d6c6c5e2c336
NedaFarrokhzad <66213038+NedaFarrokhzad@users.noreply.github.com>
2026-07-07 16:44:34 +0200

    Add files via upload

---
+++ run_sentiment_analysis.py
@@ -0,0 +1,9 @@
+"""Run the sentiment analysis pipeline and generate its chart."""
+
+from src.sentiment_analysis import run_analysis
+from src.sentiment_visualization import run_visualization
+
+
+if __name__ == "__main__":
+    run_analysis()
+    run_visualization()
